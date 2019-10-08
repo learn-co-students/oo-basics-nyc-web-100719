ddb23e28467b587c6c0edc6d56f2fa859386afa5
LainaJ <lainakarosic@gmail.com>
2019-10-08 15:34:13 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,43 @@
 
 
+class Book
+    def initialize(title)
+        @title = title
+    end
+
+
+    def title
+        @title
+    end
+
+    def author=(author)
+        @author = author
+    end
+
+    def author
+        @author
+    end
+
+    def page_count=(page_count_num)
+        @page_count = page_count_num
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def genre
+        @genre
+    end
+
+    def genre=(specific_genre)
+        @genre = specific_genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+Book.new("And Then There Were None")
